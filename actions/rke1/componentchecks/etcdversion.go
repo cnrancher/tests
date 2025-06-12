769fe7bf444e5e348a595fb839e9d3fb970c4753
StarryWang <starry.wang@suse.com>
2025-06-12 13:16:22 +0800

    PANDARIA: Fix etcd version on openEuler

--- actions/rke1/componentchecks/etcdversion.go
+++ actions/rke1/componentchecks/etcdversion.go
@@ -29,7 +29,7 @@ func CheckETCDVersion(client *rancher.Client, nodes []*nodes.Node, clusterID str
 		if etcdRole == true {
 			for _, node := range nodes {
 				if strings.Contains(node.PublicIPAddress, externalIP) {
-					command := "docker exec etcd etcdctl version"
+					command := "sudo docker exec etcd etcdctl version" // PANDARIA: Fix openEuler 24.03-LTS-SP1
 					output, err := node.ExecuteCommand(command)
 					if err != nil {
 						return []string{}, err
